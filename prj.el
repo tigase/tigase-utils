cb62a762769d710741b09c6ed288a3a745ee4529
kobit <kobit@20a39203-4b1a-0410-9ea8-f7f58976c10f>
2006-12-15 12:09:00 +0000

    Own Base64 impementation to avoid JDK-1.6 warnings and make it compatible with JAVA API (should work fine now on other JVMs)
    git-svn-id: file:///home/svn/repos/tigase-utils/trunk@394 20a39203-4b1a-0410-9ea8-f7f58976c10f

--- prj.el
+++ prj.el
@@ -7,16 +7,16 @@
    (quote
     (
      "$JAVA_HOME/jre/lib/rt.jar"
-     "$PROJECTS_HOME/tigase/xmltools/classes/"
-     "$PROJECTS_HOME/tigase/xmltools/libs/junit.jar")))
+     "$PROJECTS_HOME/tigase/utils/classes/"
+     "$PROJECTS_HOME/tigase/utils/libs/junit.jar")))
  '(jde-sourcepath
    (quote
     (
-     "$PROJECTS_HOME/tigase/xmltools/src"
-     "$PROJECTS_HOME/tigase/xmltools/tests/unittests/src"
+     "$PROJECTS_HOME/tigase/utils/src"
+     "$PROJECTS_HOME/tigase/utils/tests/unittests/src"
      "$JAVA_HOME/share/src"
      "$JAVA_HOME/share/tests/src")))
  '(jde-gen-k&r t)
- '(user-mail-address "artur.hefczyc@gmail.com")
+ '(user-mail-address "artur.hefczyc@tigase.org")
  '(indent-tabs-mode t)
 )
