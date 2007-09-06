9074c42e9b0ab141d4570e4ecad7c178ab0348e1
bmalkow <bmalkow@20a39203-4b1a-0410-9ea8-f7f58976c10f>
2007-09-06 06:45:40 +0000

    add svn:keywords and comments
    git-svn-id: file:///home/svn/repos/tigase-utils/trunk@437 20a39203-4b1a-0410-9ea8-f7f58976c10f

--- src/main/java/tigase/util/VMPCRandom.java
+++ src/main/java/tigase/util/VMPCRandom.java
@@ -1,8 +1,36 @@
+/*
+ * Tigase Jabber/XMPP Utils
+ * Copyright (C) 2004-2007 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
 package tigase.util;
 
 import java.security.SecureRandom;
 import java.security.SecureRandomSpi;
 
+/**
+ * Describe class VMPCRandom here.
+ * 
+ * 
+ * @author <a href="mailto:bmalkow@tigase.org">Bartosz Malkowski</a>
+ * @version $Rev$
+ */
 public class VMPCRandom extends SecureRandom {
 
 	public static class Spi extends SecureRandomSpi {
