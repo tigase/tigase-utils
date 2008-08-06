81d0366114bf325b74277238b6fff97906b93548
bmalkow <bmalkow@20a39203-4b1a-0410-9ea8-f7f58976c10f>
2008-08-06 10:41:17 +0000

    add Criteria and jabber:x:form implementation
    git-svn-id: file:///home/svn/repos/tigase-utils/trunk@496 20a39203-4b1a-0410-9ea8-f7f58976c10f

---
+++ src/main/java/tigase/criteria/Criteria.java
@@ -0,0 +1,39 @@
+/*  Tigase Project
+ *  Copyright (C) 2004-2007 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+package tigase.criteria;
+
+import tigase.xml.Element;
+
+/**
+ * 
+ * <p>
+ * Created: 2007-06-20 08:03:14
+ * </p>
+ * 
+ * @author bmalkow
+ * @version $Rev$
+ */
+public interface Criteria {
+
+	Criteria add(Criteria criteria);
+
+	boolean match(Element element);
+}
---
+++ src/main/java/tigase/criteria/Or.java
@@ -0,0 +1,55 @@
+/*  Tigase Project
+ *  Copyright (C) 2004-2007 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+package tigase.criteria;
+
+import tigase.xml.Element;
+
+/**
+ * 
+ * <p>
+ * Created: 2007-06-20 09:32:29
+ * </p>
+ * 
+ * @author bmalkow
+ * @version $Rev$
+ */
+public class Or implements Criteria {
+
+	private Criteria[] crits;
+
+	public Or(Criteria... criteria) {
+		this.crits = criteria;
+	}
+
+	public Criteria add(Criteria criteria) {
+		throw new RuntimeException("Or.add() is not implemented!");
+	}
+
+	public boolean match(Element element) {
+		for (Criteria c : this.crits) {
+			if (c.match(element)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
