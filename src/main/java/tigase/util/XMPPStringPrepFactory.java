024d2ebb1638c25b608f29e4b624b794c5e8e5a4
kobit <kobit@20a39203-4b1a-0410-9ea8-f7f58976c10f>
2010-01-16 20:25:04 +0000

    JavaDoc documentation added.
    git-svn-id: file:///home/svn/repos/tigase-utils/trunk@577 20a39203-4b1a-0410-9ea8-f7f58976c10f

--- src/main/java/tigase/util/XMPPStringPrepFactory.java
+++ src/main/java/tigase/util/XMPPStringPrepFactory.java
@@ -1,26 +1,30 @@
-/*
- * Tigase Jabber/XMPP Server
- * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3 of the License.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. Look for COPYING file in the top folder.
- * If not, see http://www.gnu.org/licenses/.
- * 
- * $Rev$
- * Last modified by $Author$
- * $Date$
- */
 
+/*
+* @(#)XMPPStringPrepFactory.java   2010.01.12 at 09:20:55 PST
+*
+* Tigase Jabber/XMPP Server
+* Copyright (C) 2004-2010 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, version 3 of the License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. Look for COPYING file in the top folder.
+* If not, see http://www.gnu.org/licenses/.
+*
+* $Rev$
+* Last modified by $Author$
+* $Date$
+ */
 package tigase.util;
+
+//~--- classes ----------------------------------------------------------------
 
 /**
  * Created: Dec 28, 2009 10:02:31 PM
@@ -30,8 +34,19 @@ package tigase.util;
  */
 public abstract class XMPPStringPrepFactory {
 
+	/**
+	 * Factory method for creating and returning stringprep implementation used
+	 * by the Tigase server. This factory allows for pluggable stringprep library
+	 * usage and replacing the library used by default.
+	 * @return
+	 */
 	public static XMPPStringPrepIfc getDefaultXMPPStringPrep() {
 		return new XMPPStringPrep();
 	}
-
 }
+
+
+//~ Formatted in Sun Code Convention
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
