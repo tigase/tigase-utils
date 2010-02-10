d9368f56ca5683db397d1fae995a0d040e0d4d2c
kobit <kobit@20a39203-4b1a-0410-9ea8-f7f58976c10f>
2010-02-10 20:44:54 +0000

    Changed stringprep.processor property name to stringprep-processor to follow the convention used throughout the all Tigase code.
    git-svn-id: file:///home/svn/repos/tigase-utils/trunk@583 20a39203-4b1a-0410-9ea8-f7f58976c10f

--- src/main/java/tigase/util/XMPPStringPrepFactory.java
+++ src/main/java/tigase/util/XMPPStringPrepFactory.java
@@ -46,10 +46,10 @@ import java.util.logging.Logger;
 public abstract class XMPPStringPrepFactory {
 
 	/** Field description */
-	public static final String STRINGPREP_PROCESSOR_PROP_KEY = "stringprep.processor";
+	public static final String STRINGPREP_PROCESSOR_PROP_KEY = "stringprep-processor";
 
 	/** Field description */
-	public static final String[] STRINGPREP_PROCESSORS = { "simple", "libidn", "empty" };
+	public static final String[] STRINGPREP_PROCESSORS = { "libidn", "simple", "empty" };
 
 	/** Field description */
 	public static String STRINGPREP_PROCESSOR = "simple";
@@ -65,8 +65,7 @@ public abstract class XMPPStringPrepFactory {
 	 * @return instance of XMPP Stringprep processor.
 	 */
 	public static XMPPStringPrepIfc getDefaultXMPPStringPrep() {
-		STRINGPREP_PROCESSOR = System.getProperty(STRINGPREP_PROCESSOR_PROP_KEY,
-						STRINGPREP_PROCESSOR);
+		STRINGPREP_PROCESSOR = System.getProperty(STRINGPREP_PROCESSOR_PROP_KEY, STRINGPREP_PROCESSOR);
 
 		return getXMPPStringPrep(STRINGPREP_PROCESSOR);
 	}
@@ -132,9 +131,8 @@ public abstract class XMPPStringPrepFactory {
 		try {
 			return (XMPPStringPrepIfc) Class.forName(stringprepProcessor).newInstance();
 		} catch (Exception ex) {
-			throw new IllegalArgumentException("Incorrect stringprep class name: "
-																				 + stringprepProcessor,
-																				 ex);
+			throw new IllegalArgumentException("Incorrect stringprep class name: " + stringprepProcessor,
+					ex);
 		}
 	}
 
@@ -150,12 +148,12 @@ public abstract class XMPPStringPrepFactory {
 	public static void main(String[] args) throws Exception {
 		long testTime = 30;    // 30 seconds by default
 
-		System.out.println("Each test time: " + testTime + " seconds");
+		System.out.printf("Each test time: %1$,d seconds\n", testTime);
 
 		for (String proc : STRINGPREP_PROCESSORS) {
 			long result = runTest(TimeUnit.SECONDS.toMillis(testTime), proc);
 
-			System.out.println("Stringprep processor: " + proc + ", result: " + result);
+			System.out.printf("Stringprep processor: %1$s, result: %2$,d\n", proc, result);
 		}
 	}
 
