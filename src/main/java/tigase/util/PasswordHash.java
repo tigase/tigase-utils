29c46a24cd5bc4ac870d2fe7d2c439e250389a34
kobit <kobit@20a39203-4b1a-0410-9ea8-f7f58976c10f>
2010-03-31 23:06:56 +0000

    Portable password PHP hashing implementation in Java for a better integration with Drupal, Wordpress and other systems. Nothing done yet, just empty class.
    git-svn-id: file:///home/svn/repos/tigase-utils/trunk@596 20a39203-4b1a-0410-9ea8-f7f58976c10f

---
+++ src/main/java/tigase/util/PasswordHash.java
@@ -0,0 +1,44 @@
+
+/*
+* Tigase Jabber/XMPP Server
+* Copyright (C) 2004-2010 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, version 3 of the License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. Look for COPYING file in the top folder.
+* If not, see http://www.gnu.org/licenses/.
+*
+* $Rev$
+* Last modified by $Author$
+* $Date$
+ */
+package tigase.util;
+
+//~--- classes ----------------------------------------------------------------
+
+/**
+ * Portable PHP password hashing framework rewritten in Java - code almost copied
+ * directly. This class is used for user authentication for against PHP based websites'
+ * databases.
+ *
+ * Created: Mar 29, 2010 11:49:39 PM
+ *
+ * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
+ * @version $Rev$
+ * @link http://www.openwall.com/phpass
+ */
+public class PasswordHash {}
+
+
+//~ Formatted in Sun Code Convention
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
