68ca95245014a65fd7841da39a4ace42bc1844b4
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2017-10-22 13:35:55 +0200

    #6212: moved Token implementation from tigase-extras

---
+++ src/main/java/tigase/util/Token.java
@@ -0,0 +1,158 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2017 "Tigase, Inc." <office@tigase.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ */
+package tigase.util;
+
+import tigase.xmpp.BareJID;
+
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Created by bmalkow on 21.04.2017.
+ */
+public class Token {
+
+	private static final Random RAND_GEN = new SecureRandom();
+	private BareJID jid;
+	private String random;
+	private Date timestamp;
+
+	static String copy(byte[] buff, int offset, int len) {
+		String r = "";
+
+		for (int i = offset; i < offset + len; i++) {
+			r += (char) buff[i];
+		}
+
+		return r;
+	}
+
+	public static Token create(BareJID jid) {
+		byte[] r = new byte[20];
+		RAND_GEN.nextBytes(r);
+		return create(jid, new Date(), Base64.encode(r));
+	}
+
+	public static Token create(BareJID jid, Date timestamp, String random) {
+		Token t = new Token();
+		t.jid = jid;
+		t.timestamp = timestamp;
+		t.random = random;
+		return t;
+	}
+
+	private static Token decodeTokenV1(final byte[] buff) {
+		int jidEndPos = nullPos(buff, 1);
+		int timestampEndPos = nullPos(buff, jidEndPos + 1);
+
+		String j = copy(buff, 1, jidEndPos - 1);
+		String ts = copy(buff, jidEndPos + 1, timestampEndPos - jidEndPos - 1);
+		String r = copy(buff, timestampEndPos + 1, buff.length - timestampEndPos - 1);
+
+		Token result = new Token();
+		result.random = r;
+		result.jid = BareJID.bareJIDInstanceNS(new String(Base64.decode(j), Charset.forName("UTF-8")));
+		result.timestamp = new Date(Long.parseLong(ts));
+		return result;
+	}
+
+	static int nullPos(byte[] buff, int from) {
+		for (int i = from; i < buff.length; i++) {
+			if (buff[i] == 0) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static Token parse(String encodedToken) {
+		if (encodedToken == null) {
+			throw new RuntimeException("Invalid token");
+		}
+
+		byte[] buff = Base64.decode(encodedToken);
+		if (buff == null || buff.length == 0) {
+			throw new RuntimeException("Invalid token");
+		}
+		byte tokenType = buff[0];
+		switch (tokenType) {
+			case 1:
+				return decodeTokenV1(buff);
+			default:
+				throw new RuntimeException("Unknown token");
+		}
+	}
+
+	protected byte[] getBuff() {
+		String j = Base64.encode(this.jid.toString().getBytes(Charset.forName("UTF-8")));
+		String ts = String.valueOf(timestamp.getTime());
+
+		int len = 1 + j.length() + 1 + ts.length() + 1 + random.length();
+		byte[] buff = new byte[len];
+		buff[0] = 1; // token version/type
+		byte[] tmp;
+		int idx = 1;
+
+		tmp = j.getBytes();
+		System.arraycopy(tmp, 0, buff, idx, tmp.length);
+		idx += tmp.length;
+		buff[idx++] = 0;
+
+		tmp = ts.getBytes();
+		System.arraycopy(tmp, 0, buff, idx, tmp.length);
+		idx += tmp.length;
+		buff[idx++] = 0;
+
+		tmp = random.getBytes();
+		System.arraycopy(tmp, 0, buff, idx, tmp.length);
+		idx += tmp.length;
+//		buff[idx++] = 0;
+
+		return buff;
+	}
+
+	public String getEncoded() {
+		return Base64.encode(getBuff());
+	}
+
+	public String getHash() {
+		try {
+			MessageDigest sha = MessageDigest.getInstance("SHA-256");
+			return Base64.encode(sha.digest(getBuff()));
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public BareJID getJid() {
+		return jid;
+	}
+
+	public String getRandom() {
+		return random;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+}
---
+++ src/test/java/tigase/util/TokenTest.java
@@ -0,0 +1,94 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2017 "Tigase, Inc." <office@tigase.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ */
+package tigase.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+import tigase.xmpp.BareJID;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Created by bmalkow on 21.04.2017.
+ */
+public class TokenTest {
+
+	@Test
+	public void testCopy() {
+		byte[] buff = new byte[]{(byte) 97, (byte) 98, (byte) 99, (byte) 100};
+		String s = Token.copy(buff, 1, 2);
+
+		Assert.assertEquals("bc", s);
+	}
+
+	@Test
+	public void testTokenCreation() {
+		Token t = Token.create(BareJID.bareJIDInstanceNS("a@b.c"), new Date(100), "1234567890");
+		Assert.assertEquals("AVlVQmlMbU09ADEwMAAxMjM0NTY3ODkw", t.getEncoded());
+		Assert.assertEquals("cVoryT3UDGStynBGwjMp9EpsTnIPKmSGgVv+ngZgxGk=", t.getHash());
+	}
+
+	@Test
+	public void testTokenDecoding() {
+		Token t = Token.parse("AVlVQmlMbU09ADEwMAAxMjM0NTY3ODkw");
+
+		Assert.assertEquals(BareJID.bareJIDInstanceNS("a@b.c"), t.getJid());
+		Assert.assertEquals(new Date(100), t.getTimestamp());
+		Assert.assertEquals("1234567890", t.getRandom());
+
+		String j = "\u0250\u0287o\u029E\u0250\u026F\u0250\u0283\u0250@\u0287s\u01DD\u0287.\u01DD\u0283d\u026F\u0250x\u01DD.\u026Fo\u0254";
+		Date d = new Date();
+		String r = String.valueOf((new Random()).nextLong());
+		t = Token.parse(Token.create(BareJID.bareJIDInstanceNS(j), d, r).getEncoded());
+
+		Assert.assertEquals(BareJID.bareJIDInstanceNS(j), t.getJid());
+		Assert.assertEquals(d, t.getTimestamp());
+		Assert.assertEquals(r, t.getRandom());
+	}
+
+	@Test
+	public void testTokenUnicode1() throws TigaseStringprepException, UnsupportedEncodingException {
+		Token t = Token.create(BareJID.bareJIDInstance("\u0239la@b.c"), new Date(100), "1234567890");
+		Token t2 = Token.parse(t.getEncoded());
+		Assert.assertEquals("JIDs do not match!", "\u0239la@b.c", t.getJid().toString());
+		Assert.assertEquals("JIDs do not match!", t.getJid(), t2.getJid());
+
+		Assert.assertEquals("Timestamp do not match!", 100, t.getTimestamp().getTime());
+		Assert.assertEquals("Timestamp do not match!", t.getTimestamp().getTime(), t2.getTimestamp().getTime());
+
+		Assert.assertEquals("Random do not match!", "1234567890", t.getRandom());
+		Assert.assertEquals("Random do not match!", t.getRandom(), t2.getRandom());
+	}
+
+	@Test
+	public void testTokenUnicode2() throws TigaseStringprepException, UnsupportedEncodingException {
+		Token t = Token.create(BareJID.bareJIDInstance("\u0200la\u1000@b.c"), new Date(100), "1234567890");
+		Token t2 = Token.parse(t.getEncoded());
+		Assert.assertEquals("JIDs do not match!", "\u0200la\u1000@b.c", t.getJid().toString());
+		Assert.assertEquals("JIDs do not match!", t.getJid(), t2.getJid());
+
+		Assert.assertEquals("Timestamp do not match!", 100, t.getTimestamp().getTime());
+		Assert.assertEquals("Timestamp do not match!", t.getTimestamp().getTime(), t2.getTimestamp().getTime());
+
+		Assert.assertEquals("Random do not match!", "1234567890", t.getRandom());
+		Assert.assertEquals("Random do not match!", t.getRandom(), t2.getRandom());
+	}
+}
