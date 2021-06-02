1cdb57debd10458c5fec25a990682f7cad2729e5
Wojciech Kapcia <wojtek@tigase.org>
2021-06-02 14:22:42 +0200

    configure build image

---
+++ src/test/java/tigase/util/ClassUtilTest.java
@@ -0,0 +1,39 @@
+/*
+ * Tigase XMPP Server - The instant messaging server
+ * Copyright (C) 2004 Tigase, Inc. (office@tigase.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ */
+
+package tigase.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+import tigase.annotations.DeprecationTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class ClassUtilTest {
+	@Test
+	public void test() throws IOException, ClassNotFoundException {
+		String version = System.getProperty("project.version");
+		final Set<Class<?>> classesFromClassPath = ClassUtil.getClassesFromClassPath();
+
+//		System.out.println(classesFromClassPath);
+	}
+}
